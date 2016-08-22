d58e6b834b9b911d35480987f4dd2869dad3346f
Dan Nguyen <dansonguyen@gmail.com>
2016-08-21 17:21:36 -0700

    optflag

--- wrangle/scripts/argy.py
+++ wrangle/scripts/argy.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser("This does something")
 #    parser.add_argument('infile', type=argparse.FileType('r'))
 #    parser.add_argument('--someval', type=int, help="")
+#    parser.add_argument('--optflag', action='store_true', help="optional flag")
+
     args = parser.parse_args()
 
     LOGGY.info("This is a log message")
